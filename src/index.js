9e5eceb98960c8f1c21999937a303cc6a5ad7bb2
Abindra <abindrashakya.123@gmail.com>
2023-07-11 09:05:19 +0545

    cors used to connect to react

--- src/index.js
+++ src/index.js
@@ -5,43 +5,27 @@ const todoRoutes = require('./modules/todo/todo.routes');
 const app = express();
 const router = express.Router();
 const todoController = require('./modules/todo/todo.controller')
+const axios = require('axios')
+const cors = require('cors');
 app.listen(8081, () => {
     console.log(`API is listening on port 8081`);
 });
 
+app.use(cors({
+    origin: '*'
+}));
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use('/todo',todoRoutes)
 
-router.get('/',(req, res) => {
-    res.send(`<h2>Hello from</h2>`);
-});
 
-router.get('/test',(req, res) => {
-    res.send(`<h2>Hello this is test route</h2>`);
-});
-
-router.get('/test/:id/:bd',(req, res) => {
-    res.send(`<h2>Hello this is test ${req.params.id}</h2>`);
-});
-
-router.post('/product',(req, res) => {
-    console.log(req.body);
-    res.send(`<h2>Hello this is test </h2>`);
-});
-
-router.post('/card',(req, res) => {
-    console.log(req.body);
-    res.send(`<h2>Welcome to card </h2>`);
-});
-// router.put('/product',updateProduct);
-// router.put('/card',updateCard);
-
-mongoose.connect("mongodb+srv://abindrashakya:abs12345678@firstproject.9lljnan.mongodb.net");
+// mongoose.connect("mongodb+srv://abindrashakya:abs12345678@firstproject.9lljnan.mongodb.net");
+mongoose.connect("mongodb+srv://abindra:shakya123@abindracluster.0cyegia.mongodb.net/?retryWrites=true&w=majority");
 const database = mongoose.connection;
 database.on('error', (error) => {
     console.log(error)
 });
-app.use('/todo',todoRoutes)
 // router.get('/todo', todoController.getAllTodo);
 // router.post('/todo', todoController.createTodo);
 // router.put('/todo/:id', todoController.updateTodo);
